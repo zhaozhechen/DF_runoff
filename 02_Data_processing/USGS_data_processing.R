3a0a089b5f2db023c12731f9f775b3a933059d7d
endlessczz <endlessczz@gmail.com>
2025-07-15 16:13:36 -0500

    Add initial data processing codes

---
+++ 02_Data_processing/USGS_data_processing.R
@@ -0,0 +1,11 @@
+# Author: Zhaozhe Chen
+# Update Date: 2025.7.15
+
+# -------- Gloabl -----------
+library(dplyr)
+library(lfstat) # assign dates into USGS water years
+
+# Data source
+# From USGS Data Release (https://www.sciencebase.gov/catalog/item/6696bef8d34ecb78f609f651)
+
+
