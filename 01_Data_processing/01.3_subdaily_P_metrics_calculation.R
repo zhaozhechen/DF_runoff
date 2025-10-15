cb6d50e9d6e756c137f64386f1add2f81a42377b
endlessczz <endlessczz@gmail.com>
2025-10-15 18:24:28 -0500

    Complete function to assign event ID to individual rainfall separated by at least 2 hours

--- 01_Data_processing/01.3_subdaily_P_metrics_calculation.R
+++ 01_Data_processing/01.3_subdaily_P_metrics_calculation.R
@@ -49,41 +49,14 @@ subdaily_P <- read.csv(paste0(USGS_subdaily_P_path,file_name)) %>%
   ),
   dateTime = ymd_hms(dateTime,tz="UTC"))
 
-# Separate rainfall events ================
-subdaily_P <- subdaily_P %>%
-  arrange(dateTime) %>%
-  mutate(
-    # Label dry obs
-    is_rain = Precip_Inst_mm > P_th,
-    # Get time difference between each two obs in hours
-    time_diff_hr = as.numeric(difftime(dateTime,lag(dateTime),units = "hours")),
-    # Remove the first NA in time_diff_hr
-    time_diff_hr = replace_na(time_diff_hr,0),
-    
-    # Define dry groups: each time a dry period starts, increase ID
-    dry_start = (!is_rain) & (lag(is_rain,default = TRUE)),
-    # These dry groups are consecutive dry obs without rain interruption
-    dry_group = if_else(!is_rain,cumsum(dry_start),NA)
-) %>%
-  group_by(dry_group) %>%
-  mutate(
-    # Get cumulative dry period duration in hours
-    cum_dry_time_hr = if_else(!is_rain,cumsum(time_diff_hr),NA)
-    )%>%
-  ungroup() %>%
-  mutate(
-    # Get cumulative dry duration before rain
-    dry_time_before_rain = lag(cum_dry_time_hr),
-    # Define new rainfall events, if it has been dried for time longer than MIT
-    new_event = if_else(
-      is_rain & 
-        (is.na(lag(is_rain)) | lag(is_rain) == FALSE) &
-        (dry_time_before_rain > MIT | is.na(dry_time_before_rain)),
-      TRUE,FALSE
-    ),
-    # Assign rainfall event ID
-    Rainfall_ID = if_else(is_rain,cumsum(new_event),NA)
-  )
+# Assign event ID to each unique rainfall event
+event_P <- Assign_P_event_ID(subdaily_P,P_th,MIT)
+
+
+
+  
+
+
     
     
 
--- Functions/Data_processing_functions.R
+++ Functions/Data_processing_functions.R
@@ -38,3 +38,62 @@ USGS_ppt <- function(USGS_ID,start,end){
   
   return(ppt)
 }
+
+# This function is to separate rainfall events, and give each individual event a event_ID
+# Only rainfall events separated by at least MIT (hours) are considered separate rainfall
+# P_event_ID is also assigned to dry periods within an event. i.e., between the first and last rain obs of each event
+# Input includes:
+# P_df, which should be the df of full P time series
+# P_th: threshold to define if it rains or not
+# MIT: # Minimum inter-event time (MIT) to separate rainfall event
+Assign_P_event_ID <- function(P_df,P_th,MIT){
+  event_P <- P_df %>%
+    arrange(dateTime) %>%
+    mutate(
+      # Label dry obs
+      is_rain = Precip_Inst_mm > P_th,
+      # Get time difference between each two obs in hours
+      time_diff_hr = as.numeric(difftime(dateTime,lag(dateTime),units = "hours")),
+      # Remove the first NA in time_diff_hr
+      time_diff_hr = replace_na(time_diff_hr,0),
+      
+      # Define dry groups: each time a dry period starts, increase ID
+      dry_start = (!is_rain) & (lag(is_rain,default = TRUE)),
+      # These dry groups are consecutive dry obs without rain interruption
+      dry_group = if_else(!is_rain,cumsum(dry_start),NA)
+    ) %>%
+    group_by(dry_group) %>%
+    mutate(
+      # Get cumulative dry period duration in hours
+      cum_dry_time_hr = if_else(!is_rain,cumsum(time_diff_hr),NA)
+    )%>%
+    ungroup() %>%
+    mutate(
+      # Get cumulative dry duration before rain
+      dry_time_before_rain = lag(cum_dry_time_hr),
+      # Define new rainfall events, if it has been dried for time longer than MIT
+      new_event = if_else(
+        is_rain & 
+          (is.na(lag(is_rain)) | lag(is_rain) == FALSE) &
+          (dry_time_before_rain > MIT | is.na(dry_time_before_rain)),
+        TRUE,FALSE
+      ),
+      # Assign rainfall event ID only to rain obs
+      Rainfall_ID = if_else(is_rain,cumsum(new_event),NA)
+    ) %>%
+    fill(Rainfall_ID,.direction = "down") %>%
+    # Mask out dry obs that are after the last rain obs of each event
+    group_by(Rainfall_ID) %>%
+    mutate(
+      has_rain = any(is_rain),
+      last_rain_time = max(dateTime[is_rain],na.rm=TRUE),
+      P_event_ID = if_else(dateTime > last_rain_time,NA,Rainfall_ID)
+    ) %>%
+    ungroup() %>%
+    select(dateTime,Precip_Inst_mm,P_event_ID)  
+  return(event_P)
+}
+
+
+
+
